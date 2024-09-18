3135c9d52ce5d9425455047edbba371b2c47e735
EricJansson <eric95.jansson@gmail.com>
2024-09-18 15:54:50 +0200

    created obstacle class

--- GameEngine/Main.cpp
+++ GameEngine/Main.cpp
@@ -1,7 +1,9 @@
 #include "RunWindow.h"
+#include "Obstacle.h"
 
 int main(int argc, char* args[])
 {
     RunWindow test = RunWindow();
+    Obstacle box = Obstacle(2, 2);
     return test.Run();
 }

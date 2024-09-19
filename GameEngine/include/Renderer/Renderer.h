a3d10d525fbeb9535d88aa4d2032dd2ce60cda6e
IsakSalmi <hansisak11@gmail.com>
2024-09-19 19:59:56 +0200

    added a whay to render a rect dyn

--- GameEngine/include/Renderer/Renderer.h
+++ GameEngine/include/Renderer/Renderer.h
@@ -2,6 +2,8 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <vector>
+#include <memory>
+#include <utility>
 #include "Renderer/IRenderObject.h"
 
 namespace Rend
@@ -9,20 +11,22 @@ namespace Rend
 class Renderer
 {
 public:
-    Renderer(int width, int hight, int maxFps);
+    Renderer(int width, int hight);
     ~Renderer();
 
-    void renderBackground(int red, int green, int blue, int alpha); 
+    void renderBackground(int red, int green, int blue, int alpha);
+    void renderObject();
+    void addObjectToRender(std::unique_ptr<IRenderObject> renderObject); 
 
 private:
-    SDL_Window *window;
-    SDL_Renderer *renderer;
+    void renderRect(SDL_Rect rec, SDL_Color color);
+    SDL_Window *m_window;
+    SDL_Renderer *m_renderer;
 
     const int m_width;
     const int m_hight;
-    const int m_maxFps;
 
-    std::vector<IRenderObject> objectToRender;
+    std::vector<std::unique_ptr<IRenderObject>> objectToRender;
 };
 
 } // namespace Rend
--- GameEngine/src/Renderer/Renderer.cpp
+++ GameEngine/src/Renderer/Renderer.cpp
@@ -3,38 +3,68 @@
 
 namespace Rend
 {
-
-Renderer::Renderer(int width, int hight, int maxFps)
+using namespace std;
+Renderer::Renderer(int width, int hight)
 : m_width(width)
 , m_hight(hight)
-, m_maxFps(maxFps)
+, m_window(nullptr)
+, m_renderer(nullptr)
 {
-    window = SDL_CreateWindow("SDL Window",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,m_width,m_hight,SDL_WINDOW_ALLOW_HIGHDPI);
-    if (window == nullptr)
+    m_window = SDL_CreateWindow("SDL Window",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,m_width,m_hight,SDL_WINDOW_ALLOW_HIGHDPI);
+    if (m_window == nullptr)
     {
         SDL_Log("SDL_CreateWindow Error: %s\n", SDL_GetError());
     }
 
-    renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
-    if (renderer == nullptr)
+    m_renderer = SDL_CreateRenderer(m_window,-1,SDL_RENDERER_ACCELERATED);
+    if (m_renderer == nullptr)
     {
         SDL_Log("SDL_CreateRenderer Error: %s\n", SDL_GetError());
     }
-    SDL_SetRenderDrawBlendMode(renderer,SDL_BLENDMODE_BLEND);
+    SDL_SetRenderDrawBlendMode(m_renderer,SDL_BLENDMODE_BLEND);
 }
 
 Renderer::~Renderer()
 {
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    if (m_renderer != nullptr) {
+        SDL_DestroyRenderer(m_renderer);  
+        m_renderer = nullptr;            
+    }
+    if (m_window != nullptr) {
+        SDL_DestroyWindow(m_window);      
+        m_window = nullptr;               
+    }
     SDL_Quit();
+}
+
+void Renderer::addObjectToRender(std::unique_ptr<IRenderObject> renderObject)
+{
+    objectToRender.push_back(std::move(renderObject));
+}
+
+void Renderer::renderRect(SDL_Rect rec, SDL_Color color)
+{
+    SDL_SetRenderDrawColor(m_renderer, color.r, color.g, color.b, color.a);
+    SDL_RenderFillRect(m_renderer, &rec);
+}
+
+void Renderer::renderObject()
+{
+    for(int index = 0; index <  objectToRender.size(); index++)
+    {
+        pair<int, int> pos = objectToRender[index]->getPosOfObject();
+        pair<int, int> widthAndHight = objectToRender[index]->getWidthAndhight();
+        SDL_Color color = objectToRender[index]->getColour();
+        SDL_Rect r = { pos.first, pos.second, widthAndHight.first, widthAndHight.second };
+        renderRect(r,color);
+    }
+    SDL_RenderPresent(m_renderer);
 }
 
 void Renderer::renderBackground(int red, int green, int blue, int alpha)
 {
-    SDL_SetRenderDrawColor(renderer, red, green, blue, alpha);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    SDL_SetRenderDrawColor(m_renderer, red, green, blue, alpha);
+    SDL_RenderClear(m_renderer);
 }
 
 } // namespace Rend

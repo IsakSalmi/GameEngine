41c5008ad79f2b51fe91754fe568acd5d2d60e53
IsakSalmi <hansisak11@gmail.com>
2024-09-21 12:52:30 +0200

    Added a .sh to run prodject in terminal with comand ./build_and_run.sh

--- GameEngine/Main.cpp
+++ GameEngine/Main.cpp
@@ -6,22 +6,18 @@
 #include "Entity/Entity.h"
 #include <iostream>
 
-
-const int FPS = 60;
-const int frameDelay = 1000 / FPS;
-
-
 int main(int argc, char* args[])
 {
     Obstacle box = Obstacle(2, 2);
     SDL_Init(SDL_INIT_EVERYTHING);
     Rend::Renderer rend = Rend::Renderer(1920 ,1080);
-    // Keep the main loop until the window is closed (SDL_QUIT event)
+
     bool exit = false;
     SDL_Event eventData;
 
     std::shared_ptr<Ent::Entity> temp1 = std::make_shared<Ent::Entity>();
     rend.addObjectToRender(temp1);
+
     std::shared_ptr<Ent::Entity> temp2 = std::make_shared<Ent::Entity>();
     temp2->setWidhtAndHeight({30, 500});
     temp2->setPoistionXY({500,50});
@@ -30,22 +26,25 @@ int main(int argc, char* args[])
     std::shared_ptr<Ent::Entity> temp3 = std::make_shared<Ent::Entity>();
     temp3->setWidhtAndHeight({30, 500});
     temp3->setPoistionXY({0,50});
-    temp3->setWidhtAndHeight({30, 500});
-    temp3->setPoistionXY({0,50});
     rend.addObjectToRender(temp3);
 
     bool running = true;
     Uint32 frameStart;
     int frameTime;
     int frameCount = 0;
+    const int FPS = 30;
+    const int frameDelay = 1000 / FPS;
+    Uint32 lastTime = SDL_GetTicks();  
+    Uint32 fpsTimer = 0;       
 
     Utility::ObjectUtility utility;
 
     int32_t dir = 5;
     while (running)
     {
+        frameStart = SDL_GetTicks();
+
         rend.renderBackground(255,0,0,255);
-        frameStart = SDL_GetTicks();  // Get the number of milliseconds since the SDL library was initialized
 
         // Event handling
         while (SDL_PollEvent(&eventData)) {
@@ -60,32 +59,40 @@ int main(int argc, char* args[])
                 }
             }
         }
+
         if(utility.checkCollision(temp1->getRect(), temp2->getRect())){
             dir = -5;
         }
         else if (utility.checkCollision(temp1->getRect(), temp3->getRect())) {
             dir = 5;
         }
-        else {
-        }
+
         temp1->moveXY({dir,-1});
+
         if(utility.checkCollision(temp1->getRect(), temp2->getRect())){
             dir = -5;
         }
         else if (utility.checkCollision(temp1->getRect(), temp3->getRect())) {
             dir = 5;
         }
-        else {
-        }
+
         temp1->moveXY({dir,-1});
-
-        frameTime = SDL_GetTicks() - frameStart;  // Calculate how long this frame took
-
         rend.renderObject();
+
+        frameTime = SDL_GetTicks() - frameStart;
+
+        if (frameDelay > frameTime) {
+            SDL_Delay(frameDelay - frameTime);
+        }
+
         frameCount++;
-        SDL_Delay(frameDelay - frameTime);  // Delay to fill the remaining time of the frame
-        if (frameDelay > frameTime)
-            SDL_Delay(frameDelay - frameTime); 
+
+        Uint32 currentTime = SDL_GetTicks();
+        if (currentTime - lastTime >= 1000) {
+            std::cout << "FPS: " << frameCount << std::endl;
+            frameCount = 0;
+            lastTime = currentTime;
+        }
     }
     return 0;
 }
---
+++ GameEngine/src/Physics/PhysicsCalculator.cpp
@@ -0,0 +1,6 @@
+#include "Physics/PhysicsCalculator.h"
+
+namespace physics
+{
+    
+} // namespace physics
